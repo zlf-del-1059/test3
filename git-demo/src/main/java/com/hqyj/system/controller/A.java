67a8e15d108f95ce7208daea60a32422c49676c3
zlf <408998317@qq.com>
2020-12-03 17:06:14 +0800

    修改

--- git-demo/src/main/java/com/hqyj/system/controller/A.java
+++ git-demo/src/main/java/com/hqyj/system/controller/A.java
@@ -1,8 +1,8 @@
 package com.hqyj.system.controller;
-
+a
 public class A {
     private int i = 11;
-
+    private int  = 11;
     public static void main(String[] args) {
         System.err.println("---------------测试git-------------");
     }
